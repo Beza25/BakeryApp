e9cf0929862ca0044182fb6685f0717b59227d13
rramdat95 <ryan.t.ramdat@gmail.com>
2019-09-17 14:08:45 -0400

    Remebered to add rest of goods

--- BakeGoodsDB.java
+++ BakeGoodsDB.java
@@ -18,15 +18,20 @@ public class BakeGoodsDB {
 
     public BakeGoodsDB(){
         bakeGoods.add(new BakeGoods("Banana Bread", 5.0, new Date(2019, 9, 24), 50)); //0
-        bakeGoods.add( new BakeGoods("White Bread", 8.0, new Date(2019,10,1) ,100)); //1
-        bakeGoods.add( new BakeGoods("MultiGrain Bread", 10.0, new Date(2019,9, 25),10));//2
-        bakeGoods.add( new BakeGoods("Muffins", 6.0, new Date(2019,9, 20),5));//3
-        bakeGoods.add( new BakeGoods("Cinnamon Bread ", 3.0, new Date(2019,9, 22),25));//4
-        bakeGoods.add( new BakeGoods("Savory Rolls", 2.5, new Date(2019,9, 23),15));//5
-        bakeGoods.add( new BakeGoods("Chocolate Croissant ", 6.5, new Date(2019,9, 21),80));//6
-        bakeGoods.add( new BakeGoods("Regular Croissant", 6.5 , new Date(2019,9, 20),12));             //7
-        bakeGoods.add( new BakeGoods("Brownie", 20, new Date(2019,9, 25),200));    //8
-        bakeGoods.add( new BakeGoods("Bagels", 2.5, new Date(2019,9, 30),100));     //9
+        bakeGoods.add(new BakeGoods("White Bread", 8.0, new Date(2019,10,1) ,100)); //1
+        bakeGoods.add(new BakeGoods("MultiGrain Bread", 10.0, new Date(2019,9, 25),10));//2
+        bakeGoods.add(new BakeGoods("Muffins", 6.0, new Date(2019,9, 20),5));//3
+        bakeGoods.add(new BakeGoods("Cinnamon Bread ", 3.0, new Date(2019,9, 22),25));//4
+        bakeGoods.add(new BakeGoods("Savory Rolls", 2.5, new Date(2019,9, 23),15));//5
+        bakeGoods.add(new BakeGoods("Chocolate Croissant ", 6.5, new Date(2019,9, 21),80));//6
+        bakeGoods.add(new BakeGoods("Regular Croissant", 6.5 , new Date(2019,9, 20),12));             //7
+        bakeGoods.add(new BakeGoods("Brownie", 20, new Date(2019,9, 25),200));    //8
+        bakeGoods.add(new BakeGoods("Bagels", 2.5, new Date(2019,9, 30),100));     //9
+        bakeGoods.add(new BakeGoods("Corn Bread", 75.0, new Date(2019, 9, 17), 5000)); //10
+        bakeGoods.add(new BakeGoods("Not White Bread", .1, new Date(2019,10,25) ,1000)); //11
+        bakeGoods.add(new BakeGoods("Cookies", 1.0, new Date(2019,9, 29),60));//12
+        bakeGoods.add(new BakeGoods("Cake", 600, new Date(2019,9, 24),8));//13
+        bakeGoods.add(new BakeGoods("Flat Bread", 31.0, new Date(2019,9, 30),250));//14
 
         //All of our bakegoods have dairy
         allergiesBakeGood.put("dairy", new ArrayList<BakeGoods>() {
--- Main.java
+++ Main.java
@@ -26,11 +26,14 @@ public class Main {
 
     }
 }
-
-// for(int row =0; row<50; row++){
-//        for(int col=0; col >=row; col++){
-//        if()
-//        }
+//    String str = "+"
+// for(int row =0; row<5; row++){
+//
+//        for(int col=0; col < = row; col++){
+//        if(row == 0 && row ==4){
+//          System.out.println("-")
+//        }else{
+//
 //
 //        System.out.println("");
 //        }
